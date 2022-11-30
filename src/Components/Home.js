b0d7b260d39cc333adfa7372589718d176bf2199
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-11-30 23:28:55 +0500

    Using React Hook fix the problem for multiple searches

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,13 @@
-import { useEffect } from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Card, Container } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCountries, filteredCountries } from "../Redux/reducers/city";
 import { v4 as uuidv4 } from 'uuid';
 
-let countryName;
-
-const handleChange = (event) => {
-    countryName = event.target.value;
-}
-
 const Home = () => {
+
+    const [countryName, setcountryName] = useState("");
+    
     const dispatch = useDispatch();
     const countries = useSelector((state) => state.cityReducer);
 
@@ -21,8 +18,15 @@ const Home = () => {
     return (
         <Container>
             <div className="countrySearchBox">
-                <input className="countrySearch" type="text" placeholder="Country Name" value={countryName} onChange={handleChange} />
-                <Button variant="secondary" type="button" onClick={() => dispatch(filteredCountries(countryName))} >Search</Button>
+                <input className="countrySearch" type="text" placeholder="Country Name" value={countryName} onChange={e => setcountryName(e.target.value)} />
+                <Button variant="secondary" type="button" onClick={async() => { 
+                    await dispatch(fetchCountries());
+                    dispatch(filteredCountries(countryName));
+                    }
+                } 
+                    >
+                        Search
+                    </Button>
             </div>
             <Container fluid className="containerCountries">
                 {countries.map((country) => (
