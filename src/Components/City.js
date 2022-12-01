ca0c2a8c4950aba50bf25e307a213f905b34b66e
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-12-01 15:54:39 +0500

    Fetch cities of the country selected

--- src/Components/City.js
+++ src/Components/City.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom"
+import { fetchCities } from "../Redux/reducers/cities";
+
 
 const City = () => {
+    const dispatch = useDispatch();
     let location = useLocation();
+    useEffect(() => {
+        dispatch(fetchCities(location.state.info.name));
+      }, [dispatch]);
     return(
         <div>
             This is a City page
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,13 +1,19 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import countryReducer from './reducers/countries';
+import citiesReducer from './reducers/cities';
+
 
 const rootReducer = combineReducers({
     countryReducer,
+    citiesReducer,
   });
   
   const store = configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   });
   
   export default store;
